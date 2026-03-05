6fa7add82c0621ef541d9c4d8a9e8d8db0bba6d9
Azka Wahyu <azkawahyu95@gmail.com>
2026-03-05 15:07:30 +0700

    add prisma and zod

---
+++ app/api/employees/route.ts
@@ -0,0 +1,6 @@
+import { getEmployees } from "../../../lib/services/employee/getEmployees";
+
+export async function GET() {
+  const employees = await getEmployees();
+  return Response.json(employees);
+}
