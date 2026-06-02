f12ad59defc9be27c0c9bab7cc617145df4814ed
azkawahyu95 <azkawahyu95@gmail.com>
2026-06-02 11:09:13 +0700

    fix: fixing delete user api

--- lib/services/users/deleteUser.ts
+++ lib/services/users/deleteUser.ts
@@ -1,9 +1,19 @@
 import { prisma } from "@/lib/prisma";
 
 export async function deleteUser(id: string) {
-  return prisma.users.delete({
-    where: {
-      id: id,
-    },
-  });
+  const [, , deletedUser] = await prisma.$transaction([
+    prisma.activity_logs.updateMany({
+      where: { user_id: id },
+      data: { user_id: null },
+    }),
+    prisma.documents.updateMany({
+      where: { verified_by: id },
+      data: { verified_by: null },
+    }),
+    prisma.users.delete({
+      where: { id },
+    }),
+  ]);
+
+  return deletedUser;
 }
